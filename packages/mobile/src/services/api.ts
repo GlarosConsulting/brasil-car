1682675c5436dce0c4dddafc7df85054c65f644d
André 'Dezzy <andrevcv1@gmail.com>
2021-01-12 12:15:15 -0300

    chore: Remove 'users' module

--- packages/mobile/src/services/api.ts
+++ packages/mobile/src/services/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+const nodeEnv = String(process.env.NODE_ENV);
+
+const baseURL =
+  nodeEnv === 'development'
+    ? String(process.env.APP_API_URL_DEVELOPMENT)
+    : String(process.env.APP_API_URL_PRODUCTION);
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL,
 });
 
 export default api;
