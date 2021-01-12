0223dcf8f1877423a4b2e595629e2132863d906d
André 'Dezzy <andrevcv1@gmail.com>
2021-01-12 16:52:05 -0300

    chore: Trying to generate mobile APK

---
+++ packages/mobile/src/@types/env.d.ts
@@ -0,0 +1,5 @@
+declare module '@env' {
+  export const NODE_ENV: string;
+  export const APP_API_URL_DEVELOPMENT: string;
+  export const APP_API_URL_PRODUCTION: string;
+}
--- packages/mobile/src/services/api.ts
+++ packages/mobile/src/services/api.ts
@@ -1,11 +1,12 @@
+import {
+  NODE_ENV,
+  APP_API_URL_DEVELOPMENT,
+  APP_API_URL_PRODUCTION,
+} from '@env';
 import axios from 'axios';
 
-const nodeEnv = String(process.env.NODE_ENV);
-
 const baseURL =
-  nodeEnv === 'development'
-    ? String(process.env.APP_API_URL_DEVELOPMENT)
-    : String(process.env.APP_API_URL_PRODUCTION);
+  NODE_ENV === 'development' ? APP_API_URL_DEVELOPMENT : APP_API_URL_PRODUCTION;
 
 const api = axios.create({
   baseURL,
