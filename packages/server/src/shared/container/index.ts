c58bb894528efdadfb87d8a2606ea05d500f766e
Matheus Fraga <matheuslf44@gmail.com>
2021-01-13 11:47:55 -0300

    chore: Finish cash handling routes

--- packages/server/src/shared/container/index.ts
+++ packages/server/src/shared/container/index.ts
@@ -2,10 +2,17 @@ import { container } from 'tsyringe';
 
 import './providers';
 
+import CashHandlingRepository from '@modules/cash_handling/infra/typeorm/repositories/CashHandlingRepository';
+import ICashHandlingRepository from '@modules/cash_handling/repositories/ICashHandlingRepository';
 import InspectionsRepository from '@modules/inspections/infra/typeorm/repositories/InspectionsRepository';
 import IInspectionsRepository from '@modules/inspections/repositories/IInspectionsRepository';
 
 container.registerSingleton<IInspectionsRepository>(
   'InspectionsRepository',
   InspectionsRepository,
+);
+
+container.registerSingleton<ICashHandlingRepository>(
+  'CashHandlingRepository',
+  CashHandlingRepository,
 );
--- packages/server/src/shared/infra/http/routes/index.ts
+++ packages/server/src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import cashHandlingRouter from '@modules/cash_handling/infra/http/routes/cash_handling.routes';
 import inspectionsRouter from '@modules/inspections/infra/http/routes/inspections.routes';
 
 const routes = Router();
 
 routes.use('/inspections', inspectionsRouter);
+routes.use('/cash-handling', cashHandlingRouter);
 
 routes.get('/', (_request, response) =>
   response.json({
