1a24e703f4347a174c8b1876bb0a8e037c6de5a9
André 'Dezzy <andrevcv1@gmail.com>
2021-01-14 11:59:24 -0300

    feat(mobile): Add drawer menu

--- packages/mobile/src/pages/Authentication/index.tsx
+++ packages/mobile/src/pages/Authentication/index.tsx
@@ -3,10 +3,10 @@ import { Text } from 'react-native';
 
 import { Container } from './styles';
 
-const Home: React.FC = () => (
+const Authentication: React.FC = () => (
   <Container>
-    <Text>Home</Text>
+    <Text>Authentication</Text>
   </Container>
 );
 
-export default Home;
+export default Authentication;
