9da25a21613208cd9f828b74ccf7c8f3ef0cfcff
André 'Dezzy <andrevcv1@gmail.com>
2021-01-19 13:39:14 -0300

    fix: Migration 'AddLimitDateFieldToInspection'

--- packages/server/src/shared/infra/typeorm/migrations/1611064017347-AddLimitDateFieldToInspection.ts
+++ packages/server/src/shared/infra/typeorm/migrations/1611064017347-AddLimitDateFieldToInspection.ts
@@ -1,4 +1,7 @@
+import { addDays, endOfDay } from 'date-fns';
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+import Inspection from '@modules/inspections/infra/typeorm/entities/Inspection';
 
 export default class AddLimitDateFieldToInspection1611064017347
   implements MigrationInterface {
@@ -8,6 +11,27 @@ export default class AddLimitDateFieldToInspection1611064017347
       new TableColumn({
         name: 'limit_date',
         type: 'timestamp with time zone',
+        isNullable: true,
+      }),
+    );
+
+    const repository = queryRunner.manager.getRepository(Inspection);
+
+    const inspections = await repository.find();
+
+    for (const inspection of inspections) {
+      inspection.limit_date = addDays(endOfDay(inspection.created_at), 3);
+
+      await repository.save(inspection);
+    }
+
+    await queryRunner.changeColumn(
+      'inspections',
+      'limit_date',
+      new TableColumn({
+        name: 'limit_date',
+        type: 'timestamp with time zone',
+        isNullable: false,
       }),
     );
   }
