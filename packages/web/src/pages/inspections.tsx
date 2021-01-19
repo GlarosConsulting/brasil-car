44199cc056eaee474ae8c46ebcafbdd63f17ee99
André 'Dezzy <andrevcv1@gmail.com>
2021-01-19 20:55:52 -0300

    chore: Refactor 'cash-handling' page

--- packages/web/src/pages/inspections.tsx
+++ packages/web/src/pages/inspections.tsx
@@ -6,7 +6,7 @@ import { Column } from 'react-table';
 import { Box, Button, Flex, Text, Tooltip } from '@chakra-ui/core';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-import { format, isWithinInterval } from 'date-fns';
+import { format } from 'date-fns';
 
 import DatePicker from '@/components/DatePicker';
 import Header from '@/components/Header';
@@ -14,7 +14,6 @@ import Select from '@/components/Select';
 import SEO from '@/components/SEO';
 import Sidebar from '@/components/Sidebar';
 import Table from '@/components/Table';
-import inspectionsData from '@/mocks/Inspections';
 import api from '@/services/api';
 import getStatusFromInspections from '@/utils/getStatusFromInspections';
 
