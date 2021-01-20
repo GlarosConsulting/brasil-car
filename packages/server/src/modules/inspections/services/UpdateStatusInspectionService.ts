3fcb3b4a2c259eda91edf08c49f61d32f1ef0948
Matheus Fraga <matheuslf44@gmail.com>
2021-01-20 14:06:12 -0300

    chore: Create update inspections status route

---
+++ packages/server/src/modules/inspections/services/UpdateStatusInspectionService.ts
@@ -0,0 +1,40 @@
+import { merge } from 'lodash';
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Inspection, {
+  Status,
+} from '@modules/inspections/infra/typeorm/entities/Inspection';
+import IInspectionsRepository from '@modules/inspections/repositories/IInspectionsRepository';
+
+interface IRequest {
+  id: string;
+  status: Status;
+}
+
+@injectable()
+class UpdateStatusInspectionService {
+  constructor(
+    @inject('InspectionsRepository')
+    private inspectionsRepository: IInspectionsRepository,
+  ) {}
+
+  public async execute({ id, status }: IRequest): Promise<Inspection> {
+    const inspection = await this.inspectionsRepository.findById(id);
+
+    if (!inspection) {
+      throw new AppError('Inspection not found', 404);
+    }
+
+    if (status !== 'pending' && status !== 'refused' && status !== 'approved') {
+      throw new AppError('Invalid status.');
+    }
+
+    merge(inspection, { status });
+
+    return this.inspectionsRepository.save(inspection);
+  }
+}
+
+export default UpdateStatusInspectionService;
