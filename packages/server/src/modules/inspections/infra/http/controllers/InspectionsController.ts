136b7b62feb5a03e143f083da0f025ad13f31cbe
Matheus Fraga <matheuslf44@gmail.com>
2021-01-22 11:48:41 -0300

    chore: Add new fields to inspections table

--- packages/server/src/modules/inspections/infra/http/controllers/InspectionsController.ts
+++ packages/server/src/modules/inspections/infra/http/controllers/InspectionsController.ts
@@ -28,17 +28,53 @@ export default class InspectionsController {
 
     const createInspection = container.resolve(CreateInspectionService);
 
+    const isFilled = (file: Express.Multer.File[]) =>
+      file ? file[0].filename : undefined;
+
     const inspection = await createInspection.execute({
       user_id,
       filenames: {
-        forward: files.forward[0].filename,
-        croup: files.croup[0].filename,
-        left_side: files.left_side[0].filename,
-        right_side: files.right_side[0].filename,
-        motor: files.motor[0].filename,
-        chassi: files.chassi[0].filename,
-        document: files.document[0].filename,
-        panel: files.panel[0].filename,
+        forward: isFilled(files.forward),
+        croup: isFilled(files.croup),
+        left_side: isFilled(files.left_side),
+        right_side: isFilled(files.right_side),
+        motor: isFilled(files.motor),
+        chassi: isFilled(files.chassi),
+        document: isFilled(files.document),
+        panel: isFilled(files.panel),
+        forward_left: isFilled(files.forward_left),
+        forward_right: isFilled(files.forward_right),
+        rear_left: isFilled(files.rear_left),
+        rear_right: isFilled(files.rear_right),
+        forward_right_with_opened_hood: isFilled(
+          files.forward_right_with_opened_hood,
+        ),
+        forward_left_with_opened_hood: isFilled(
+          files.forward_left_with_opened_hood,
+        ),
+        forward_with_opened_hood: isFilled(files.forward_with_opened_hood),
+        rear_plate: isFilled(files.rear_plate),
+        opened_trunk: isFilled(files.opened_trunk),
+        seal_plate: isFilled(files.seal_plate),
+        spare_tire: isFilled(files.spare_tire),
+        key: isFilled(files.key),
+        forward_right_wheel: isFilled(files.forward_right_wheel),
+        forward_left_wheel: isFilled(files.forward_left_wheel),
+        rear_left_wheel: isFilled(files.rear_left_wheel),
+        rear_right_wheel: isFilled(files.rear_right_wheel),
+        left_column: isFilled(files.left_column),
+        right_column: isFilled(files.right_column),
+        pedometer: isFilled(files.pedometer),
+        forward_right_tire: isFilled(files.forward_right_tire),
+        forward_left_tire: isFilled(files.forward_left_tire),
+        rear_right_tire: isFilled(files.rear_right_tire),
+        rear_left_tire: isFilled(files.rear_left_tire),
+        console: isFilled(files.console),
+        engine_number: isFilled(files.engine_number),
+        forward_right_buffer: isFilled(files.forward_right_buffer),
+        forward_left_buffer: isFilled(files.forward_left_buffer),
+        rear_right_buffer: isFilled(files.rear_right_buffer),
+        rear_left_buffer: isFilled(files.rear_left_buffer),
       },
     });
 
--- packages/server/src/modules/inspections/services/CreateInspectionService.ts
+++ packages/server/src/modules/inspections/services/CreateInspectionService.ts
@@ -9,17 +9,45 @@ import IInspectionsRepository from '@modules/inspections/repositories/IInspectio
 interface IRequest {
   user_id: string;
   filenames: {
-    forward: string;
-    croup: string;
-    left_side: string;
-    right_side: string;
-    motor: string;
-    chassi: string;
-    document: string;
-    panel: string;
+    forward?: string;
+    croup?: string;
+    left_side?: string;
+    right_side?: string;
+    motor?: string;
+    chassi?: string;
+    document?: string;
+    panel?: string;
+    forward_left?: string;
+    forward_right?: string;
+    rear_left?: string;
+    rear_right?: string;
+    forward_right_with_opened_hood?: string;
+    forward_left_with_opened_hood?: string;
+    forward_with_opened_hood?: string;
+    rear_plate?: string;
+    opened_trunk?: string;
+    seal_plate?: string;
+    spare_tire?: string;
+    key?: string;
+    forward_right_wheel?: string;
+    forward_left_wheel?: string;
+    rear_left_wheel?: string;
+    rear_right_wheel?: string;
+    left_column?: string;
+    right_column?: string;
+    pedometer?: string;
+    forward_right_tire?: string;
+    forward_left_tire?: string;
+    rear_right_tire?: string;
+    rear_left_tire?: string;
+    console?: string;
+    engine_number?: string;
+    forward_right_buffer?: string;
+    forward_left_buffer?: string;
+    rear_right_buffer?: string;
+    rear_left_buffer?: string;
   };
 }
-
 @injectable()
 class CreateInspectionService {
   constructor(
@@ -31,6 +59,54 @@ class CreateInspectionService {
   ) {}
 
   public async execute({ user_id, filenames }: IRequest): Promise<Inspection> {
+    const allFilenames: Array<keyof typeof filenames> = [
+      'forward',
+      'croup',
+      'left_side',
+      'right_side',
+      'motor',
+      'chassi',
+      'document',
+      'panel',
+      'forward_left',
+      'forward_right',
+      'rear_left',
+      'rear_right',
+      'forward_right_with_opened_hood',
+      'forward_left_with_opened_hood',
+      'forward_with_opened_hood',
+      'rear_plate',
+      'opened_trunk',
+      'seal_plate',
+      'spare_tire',
+      'key',
+      'forward_right_wheel',
+      'forward_left_wheel',
+      'rear_left_wheel',
+      'rear_right_wheel',
+      'left_column',
+      'right_column',
+      'pedometer',
+      'forward_right_tire',
+      'forward_left_tire',
+      'rear_right_tire',
+      'rear_left_tire',
+      'console',
+      'engine_number',
+      'forward_right_buffer',
+      'forward_left_buffer',
+      'rear_right_buffer',
+      'rear_left_buffer',
+    ];
+
+    const filledFiles = allFilenames.filter(
+      file => filenames[file] !== undefined,
+    );
+
+    const filledFilenames = filledFiles.map(file =>
+      this.storageProvider.saveFile(filenames[file] as string),
+    );
+
     const [
       forwardFilename,
       croupFilename,
@@ -40,16 +116,7 @@ class CreateInspectionService {
       chassiFilename,
       documentFilename,
       panelFilename,
-    ] = await Promise.all([
-      this.storageProvider.saveFile(filenames.forward),
-      this.storageProvider.saveFile(filenames.croup),
-      this.storageProvider.saveFile(filenames.left_side),
-      this.storageProvider.saveFile(filenames.right_side),
-      this.storageProvider.saveFile(filenames.motor),
-      this.storageProvider.saveFile(filenames.chassi),
-      this.storageProvider.saveFile(filenames.document),
-      this.storageProvider.saveFile(filenames.panel),
-    ]);
+    ] = await Promise.all(filledFilenames);
 
     const limitDate = addDays(endOfDay(Date.now()), 3);
 
