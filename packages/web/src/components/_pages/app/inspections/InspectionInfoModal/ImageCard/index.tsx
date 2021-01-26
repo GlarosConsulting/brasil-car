0c2eb6ddce1554fd27b9b89bcd3cb690d81fc4c4
Matheus Fraga <matheuslf44@gmail.com>
2021-01-26 18:06:13 -0300

    chore: Fix bugs on 'InspectionInfoModal'

--- packages/web/src/components/_pages/app/inspections/InspectionInfoModal/ImageCard/index.tsx
+++ packages/web/src/components/_pages/app/inspections/InspectionInfoModal/ImageCard/index.tsx
@@ -6,7 +6,7 @@ import { transparentize } from 'polished';
 interface IImageCardProps {
   id?: string;
   title: string;
-  image_url: string;
+  image_url: string | null;
 }
 
 const ImageCard: React.FC<IImageCardProps> = ({ id, title, image_url }) => {
@@ -17,27 +17,31 @@ const ImageCard: React.FC<IImageCardProps> = ({ id, title, image_url }) => {
   }, []);
 
   return (
-    <Box
-      id={id || ''}
-      position="relative"
-      borderRadius="md"
-      overflow="hidden"
-      cursor="pointer"
-      onClick={handleOpenImage}
-    >
-      <Image src={image_url} />
+    <>
+      {image_url && (
+        <Box
+          id={id || ''}
+          position="relative"
+          borderRadius="md"
+          overflow="hidden"
+          cursor="pointer"
+          onClick={handleOpenImage}
+        >
+          <Image src={image_url} />
 
-      <Flex
-        position="absolute"
-        bottom={0}
-        justifyContent="center"
-        alignItems="center"
-        bg={transparentize(0.25, theme.colors.white)}
-        width="100%"
-      >
-        {title}
-      </Flex>
-    </Box>
+          <Flex
+            position="absolute"
+            bottom={0}
+            justifyContent="center"
+            alignItems="center"
+            bg={transparentize(0.25, theme.colors.white)}
+            width="100%"
+          >
+            {title}
+          </Flex>
+        </Box>
+      )}
+    </>
   );
 };
 
